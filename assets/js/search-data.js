eeae182a597f7b9ef39ec67cfa869f77b90c1b81
yarakyrychenko <70731529+yarakyrychenko@users.noreply.github.com>
2025-02-20 22:20:48 +0000

    Deploying to gh-pages from @ yarakyrychenko/yarakyrychenko.github.io@fde88e9628491d7d37982bb0a29e190d037ca824 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -95,6 +95,41 @@ ninja.data = [{
           window.open("yarakyrychenko.bsky.social", "_blank");
         },
       },{
+        id: 'social-email',
+        title: 'email',
+        section: 'Socials',
+        handler: () => {
+          window.open("mailto:%79%6B%34%30%38@%63%61%6D.%61%63.%75%6B", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/yarakyrychenko", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0000-0003-0636-5046", "_blank");
+        },
+      },{
+        id: 'social-rss',
+        title: 'RSS Feed',
+        section: 'Socials',
+        handler: () => {
+          window.open("/feed.xml", "_blank");
+        },
+      },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=SvE3uOwAAAAJ", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
